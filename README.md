8e56fc7a059d51121e14b4e2f223e2543b93b565
Gchustz <george.chustz@gmail.com>
2018-09-13 13:56:45 -0500

    Finished setup.sh

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ##############################################################################
 AUTOCONE FALL 2018
 ##############################################################################
+Documentation on the code in this repository:
 
+>>setup.sh : 	script for installing all the necessary python packages to
+		properly interface with the pixracer. 
---
+++ setup.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-autoremove
+
+sudo apt-get install python-pip -y
+sudo apt-get install python-dev -y
+sudo pip install future
+
+sudo pip install screen
+sudo pip install wxgtk3.0
+sudo pip install python-matplotlib
+sudo pip install python-opencv
+sudo pip install python-numpy
+sudo pip install libxml2-dev
+sudo pip install libxslt-dev
+sudo pip install serial
+sudo pip install dronekit
+
