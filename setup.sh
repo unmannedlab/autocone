45a38dcf8022e44d2cd7797a6b32b7a08332c38b
Gchustz <george.chustz@gmail.com>
2018-09-24 11:57:39 -0500

    Added ros kinetic install on setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,22 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-autoremove
 
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-get install ros-kinetic-desktop-full
+sudo rosdep init
+rosdep update
+
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
+
 sudo apt-get install python-pip -y
 sudo apt-get install python-dev -y
 sudo pip install future
@@ -18,3 +34,7 @@ sudo pip install dronekit
 sudo pip install multiprocessing
 sudo pip install socket
 
+
+
+
+sudo reboot
