352c2bc6cf0676a82a1bf1cba22d362ce2207220
George Chustz <34346878+gchustz@users.noreply.github.com>
2020-10-07 12:27:59 -0500

    added some launch files

---
+++ src/veh_hdg_vel_cont.py
@@ -0,0 +1,140 @@
+import rospy
+import time
+import geometry
+
+# Import ros messages
+from std_msgs.msg import *
+from geometry_msgs.msg import *
+from ubxtranslator.msg import relpos2D
+
+# Import controller items
+from heading_rate_controller import PID_hdg
+from vehicle_transforms import tf_skid_steering
+
+class ros_hdg_vel_controller:
+    """
+    """
+    def __init__(
+        self,
+        heading_controller,
+        vehicle_transform,
+        node_name,
+        rover_pos_topic,
+        rover_pos_type,
+        left_velocity_topic,
+        right_velocity_topic,
+        velocity_type,
+        command_vel_topic,
+        command_hdg_topic,
+        command_type
+        ):
+
+        # Initialize the rospy node
+        rospy.init_node(node_name)
+
+        # Initialize internal objects
+        self.hc = heading_controller
+        self.tf = vehicle_transform
+
+        # Initialize internal variables
+        self.rov_theta_current = None 
+        self.rov_cmd_vel_current = None
+        self.rov_cmd_hdg_current = None
+        self.rover_pos_topic = rover_pos_topic
+        self.rover_pos_type = rover_pos_type
+        self.node_name = node_name
+        self.lt_topic = left_velocity_topic
+        self.rt_topic = right_velocity_topic
+        self.vel_type = velocity_type
+        self.cmd_topic_vel = command_vel_topic
+        self.cmd_topic_hdg = command_hdg_topic
+        self.cmd_type = command_type
+        self.time_start = time.time()
+
+        # Initialize check variables
+        self.rov_pos_new = False
+        self.cmd_vel_new = False 
+        self.cmd_hdg_new = False 
+        
+        # Initialize left and right publishers
+        self.pub_lt = rospy.Publisher(self.lt_topic, self.vel_type, queue_size=10)
+        self.msg_lt = self.vel_type()
+        self.pub_rt = rospy.Publisher(self.rt_topic, self.vel_type, queue_size=10)
+        self.msg_rt = self.vel_type()
+
+    def callback_rov_pos(
+        self,
+        data
+        ):
+        self.rov_pos_new = True
+        #rospy.loginfo('Got the rover {}'.format(data))
+        self.rov_theta_current = geometry.deg2rad(data.pos.theta)
+    
+    def callback_cmd_vel(
+        self,
+        data    
+        ):
+        self.cmd_vel_new = True
+        #rospy.loginfo('Got the vel cmd {}'.format(data))
+        self.rov_cmd_vel_current = data.data
+    
+    def callback_cmd_hdg(
+        self,
+        data
+        ):
+        #rospy.loginfo('Got the hdg cmd {}'.format(data))
+        self.rov_cmd_hdg_current = geometry.deg2rad(data.data)
+	self.cmd_hdg_new = True
+
+        # Peform update commands
+        if self.rov_pos_new and self.cmd_vel_new and self.cmd_hdg_new and self.rov_theta_current != None and self.rov_cmd_vel_current != None and self.rov_cmd_hdg_current != None :
+            t = time.time() - self.time_start
+            hdg_rate = self.hc.step( self.rov_cmd_hdg_current, self.rov_theta_current, t)
+            (rvel, lvel) = self.tf.transform( hdg_rate, self.rov_cmd_vel_current)
+
+            rospy.loginfo('Sending the following commands \n target heading: {} \n target velocity: {} heading_rate: {} \n left/right velocity : {} / {}'.format(self.rov_cmd_hdg_current, self.rov_cmd_vel_current, hdg_rate, rvel, lvel))
+            self.msg_lt.data = lvel 
+            self.msg_rt.data = rvel 
+
+            self.pub_lt.publish( self.msg_lt )
+            self.pub_rt.publish( self.msg_rt )
+
+        self.rov_pos_new = False
+        self.cmd_vel_new = False 
+        self.cmd_hdg_new = False 
+        
+    def listener( self ) :
+        # Start the subscribers
+        rospy.Subscriber(self.rover_pos_topic, self.rover_pos_type, self.callback_rov_pos)
+        rospy.Subscriber(self.cmd_topic_vel, self.cmd_type, self.callback_cmd_vel)
+        rospy.Subscriber(self.cmd_topic_hdg, self.cmd_type, self.callback_cmd_hdg)
+
+        # Use rospy spin to hold the node open or whatever
+        rospy.spin()
+
+if __name__=='__main__' :
+    # This is going to run the heading / velocity control loop
+    kp = 2.5
+    ki = 0.0
+    kd = 0.1
+
+    # Initialize the heading controller and the transform
+    hc = PID_hdg(kp, ki, kd)
+
+    tf = tf_skid_steering( 0.5, 0.254 )
+
+    controller = ros_hdg_vel_controller(
+        heading_controller=hc,
+        vehicle_transform=tf,
+        node_name='rover_vel_hdg_controller',
+        rover_pos_topic='/UBX/relpos2D',
+        rover_pos_type=relpos2D,
+        left_velocity_topic='/left_wheel_velocity_controller/command',
+        right_velocity_topic='/right_wheel_velocity_controller/command',
+        velocity_type=Float64,
+        command_vel_topic='/rov_cmd/vel',
+        command_hdg_topic='/rov_cmd/hdg',
+        command_type=Float64
+    )
+
+    controller.listener()    
---
+++ src/veh_spline_pub.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+
+# Create a ros subscribing/publishing loop for the spline controller
+import sys, os, time
+import numpy as np 
+import pandas as pd 
+from spline_controller import spline_controller
+import geometry
+import utm
+
+# Import ros messages
+from std_msgs.msg import *
+from geometry_msgs.msg import * 
+from ubxtranslator.msg import relpos2D, llh
+
+
+# Create a class which subscribes to the rover pos, gps coordinates, and the source gps coordinates and publishes velocity and heading commands for the rover
+class ros_spline_mgr:
+    """
+    """
+    def __init__(
+        self,
+        spline_controller,
+        node_name,
+        source_gps_topic,
+        source_gps_type,
+        rover_gps_topic,
+        rover_gps_type,
+        rover_pos_topic,
+        rover_pos_type,
+        command_vel_topic,
+        command_hdg_topic,
+        command_type
+        ):
+
+        # initialize ros node:
+        rospy.init_node(node_name)
+
+        # Save internal components:
+        self.sc = spline_controller
+        
+        # Create messages:
+        self.cmd_vel_msg = command_type()
+        self.cmd_hdg_msg = command_type()
+
+        # Create publishers:
+        self.cmd_vel_pub = rospy.Publisher(command_vel_topic, command_type, queue_size=None)
+        self.cmd_hdg_pub = rospy.Publisher(command_hdg_topic, command_type, queue_size=None)
+
+        # Save parameters for the subscriber loop
+        self.src_gps_topic = source_gps_topic
+        self.src_gps_type = source_gps_type
+        self.rov_gps_topic = rover_gps_topic
+        self.rov_gps_type = rover_gps_type
+        self.rov_pos_topic = rover_pos_topic
+        self.rov_pos_type = rover_pos_type
+
+        # Other variables for logic flow:
+        self.rov_theta_current = False
+        self.rov_theta = None
+
+    def callback_src_gps( self, data ):
+        # Add the source gps point to the data stack of the rover controller
+
+        # perform the loginfo rospy
+        rospy.loginfo('Received the source gps point: \n{}'.format(data))
+        # Define src_x, src_y, time:
+        src_lat = data.llh.lat 
+        src_lon = data.llh.lon 
+        t = time.time() 
+
+        # Add the gps point into the spline controller
+        self.sc.src_add(src_lat, src_lon, t)
+    
+    def callback_rov_pos( self, data ):
+
+        # Perform the rospy loginfo:
+        rospy.loginfo('Received the rover position: {}'.format(data))
+
+        # Set the theta and the current vars
+        self.rov_theta = geometry.deg2rad(data.pos.theta)
+        self.rov_theta_current = True 
+    
+    def callback_rov_gps( self, data) :
+        # Produce the rover feedback commands:
+
+        # Perform rospy loginfo
+        rospy.loginfo('Received the rover gps: {}'.format(data))
+        
+        # Define parameters from the incoming message
+        rov_lat = data.llh.lat
+        rov_lon = data.llh.lon 
+        rov_theta = self.rov_theta
+        t = time.time()
+
+        self.rov_theta_current = False 
+
+        # Produce feedback control:
+        (hdg_targ, vel_targ, lateness, halt) = self.sc.controller_step( t, rov_lat, rov_lon, rov_theta )
+
+        # Create messages:
+        self.cmd_hdg_msg.data = hdg_targ
+        self.cmd_vel_msg.data = vel_targ 
+
+        # Create rospy loginfo
+        rospy.loginfo('Publishing target heading and velocity: {} and {}'.format(self.cmd_hdg_msg, self.cmd_vel_msg))
+        
+        # Publish commands
+        self.cmd_vel_pub.publish(self.cmd_vel_msg)
+        self.cmd_hdg_pub.publish(self.cmd_hdg_msg)
+
+    def listener( self ) :
+
+        # Create the subscribers:
+        rospy.Subscriber(self.src_gps_topic, self.src_gps_type, self.callback_src_gps)
+        rospy.Subscriber(self.rov_pos_topic, self.rov_pos_type, self.callback_rov_pos)
+        rospy.Subscriber(self.rov_gps_topic, self.rov_gps_type, self.callback_rov_gps)
+
+        # Create a rospy spin to keep the loop from closing:
+        rospy.spin()
+
+
+
+# Run the above if main:
+if __name__ == '__main__' :
+
+    # initialize the spline controller:
+    sc = spline_controller(
+        geometry.target_heading_function,
+        'llh',
+        'llh',
+        4.0,
+        1.0,
+        1.0,
+        1.0,
+        5.0,
+        5,
+        10000
+    )
+
+    # create ros mgr
+    ros_mgr = ros_spline_mgr(
+        sc,
+        'ros_rover_mgr_node',
+        '/src_UBX/hpposllh',
+        llh,
+        '/UBX/hpposllh',
+        llh,
+        '/UBX/relpos2D',
+        relpos2D,
+        '/rov_cmd/vel',
+        '/rov_cmd/hdg',
+        Float64
+    )
+
+    ros_mgr.listener()
