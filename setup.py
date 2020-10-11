49e60bd8dd1a9bd9d199bd6d4f5bb11a784d57a3
George Chustz <34346878+gchustz@users.noreply.github.com>
2020-10-11 12:51:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 
 setup_args = generate_distutils_setup(
-    name='autocone',
+    name='autocone_tools',
     version='0.0.1',
     author='George Chustz',
     author_email='gchustz@tamu.edu',
