286f9e60c031034ce5f7617ac90900d23b4ff9d3
George Chustz <34346878+gchustz@users.noreply.github.com>
2020-10-11 12:46:09 -0500

    Attempting to get python packaging to work for launch files

--- setup.py
+++ setup.py
@@ -11,10 +11,20 @@ def readme():
 
 setup_args = generate_distutils_setup(
     name='autocone',
-    packages='autocone',
+    version='0.0.1',
+    author='George Chustz',
+    author_email='gchustz@tamu.edu',
+    description='A package which implements a simple spline control algorithm on a rover',
+    long_description=readme(),
+    url="https://github.com/unmannedlab/autocone",
+    packages='autocone_tools',
     package_dir={'' : 'src'},
-    test_suite='nose.collector',
-    tests_require=['nose']
+    classifiers=[
+        'Development Status :: 0 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Topic :: Robotics'
+    ]
     )
 
 setup(**setup_args)
