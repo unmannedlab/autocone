c0420a0a9ecbdc62d324234edd380d14fe92b686
George Chustz <34346878+gchustz@users.noreply.github.com>
2020-10-15 21:46:19 -0500

    Update geometry.py

--- src/autocone/geometry.py
+++ src/autocone/geometry.py
@@ -175,7 +175,7 @@ def target_heading_function(
     hdg_error = angle_min_diff( rov_heading, closest_point_heading )
     brg = bearing( (rov_x, rov_y), (closest_point_x, closest_point_y))
     dist = np.sin( angle_min_diff( closest_point_heading,brg )) * distance( (closest_point_x, closest_point_y), (rov_x, rov_y))
-    err  = (np.math.atan(dist) + hdg_error)/4
+    err  = np.math.atan(dist)/4 + hdg_error
 
     targ_hdg = angle_std(rov_heading+err)
 
